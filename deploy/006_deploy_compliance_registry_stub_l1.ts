03d90ced65759da2dd8355640653261e9dcddf1f
analystlee <analystlee2@gmail.com>
2024-04-14 22:23:56 +0800

    deploy and add_l1_proposal scripts

--- deploy/006_deploy_compliance_registry_stub_l1.ts
+++ deploy/006_deploy_compliance_registry_stub_l1.ts
@@ -1,18 +1,26 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import {ComplianceRegistry} from "../typechain-types";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, deployments, ethers } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  const white = await ethers.getContract("WhiteComplianceRegistry");
-  const black = await ethers.getContract("BlackComplianceRegistry");
-  await deploy("ComplianceRegistryStub_L1", {
+  const white = (await ethers.getContract("WhiteComplianceRegistry")) as ComplianceRegistry;
+  const black = (await ethers.getContract("BlackComplianceRegistry")) as ComplianceRegistry;
+  const deployedResult = await deploy("ComplianceRegistryStub_L1", {
     from: deployer,
     args: [await white.getAddress(), await black.getAddress()],
     log: true,
   });
+  const tx0 = await white.grantRole(await white.COMPLIANCE_REGISTRY_STUB_ROLE(), deployedResult.address);
+  await tx0.wait()
+  const tx1 = await black.grantRole(await white.COMPLIANCE_REGISTRY_STUB_ROLE(), deployedResult.address);
+  await tx1.wait()
+  const stub = await ethers.getContractAt('ComplianceRegistryStub_L1', deployedResult.address)
+  const tx2 = await stub.grantRole(await stub.GUARDIAN_NODE(), deployer);
+  await tx2.wait()
 };
 
 func.id = "deploy_compliance_registry_stub_l1";
