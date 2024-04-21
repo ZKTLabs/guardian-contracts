b4cfd38d978569c4a65b9a5f33e64b001185060d
analystlee <analystlee2@gmail.com>
2024-04-21 16:00:00 +0800

    deploy

--- deploy/006_deploy_multi_index.ts
+++ deploy/006_deploy_multi_index.ts
@@ -10,33 +10,36 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const {deploy} = deployments;
     const {deployer} = await getNamedAccounts();
 
-    const registryFactory = (await ethers.getContract(
-        "RegistryFactory"
-    )) as RegistryFactory;
     const registryIndexFactory = (await ethers.getContract(
         "RegistryIndexFactory"
     )) as RegistryIndexFactory;
     const stub = (await ethers.getContract("ComplianceRegistryStub_L1")) as ComplianceRegistryStub_L1;
     const BASE_MODE = toNumber(await stub.BASE_MODE());
     const stubAddress = await stub.getAddress();
-    let registryIndexArray = [];
-    /// index
-    for (let i = 0; i < BASE_MODE; i++) {
-        const address = await registryIndexFactory.deploy.staticCall(
-            i,
-            stubAddress
-        );
-        registryIndexArray.push(address)
+    let jsonObj = fs.readFileSync("registryIndexArray.json");
+    let registryIndexArray = JSON.parse(jsonObj.toString());
+    if (registryIndexArray.length == 0) {
+        throw new Error("registryIndexArray is empty");
     }
-    let jsonData = JSON.stringify(registryIndexArray, null, 2);
-    fs.writeFileSync("registryIndexArray.json", jsonData)
-    for (let i = 40; i < BASE_MODE; i++) {
-        const tx = await registryIndexFactory.deploy(i, stubAddress);
+    // let registryIndexArray = [];
+    // /// index
+    // for (let i = 0; i < BASE_MODE; i++) {
+    //     const address = await registryIndexFactory.deploy.staticCall(
+    //         i,
+    //         stubAddress
+    //     );
+    //     registryIndexArray.push(address)
+    // }
+    // let jsonData = JSON.stringify(registryIndexArray, null, 2);
+    // fs.writeFileSync("registryIndexArray.json", jsonData)
+    for (let i = 73; i < BASE_MODE; i++) {
+        const tx = await registryIndexFactory.deploy(i, stubAddress, {gasPrice: 15544294392});
         await tx.wait()
+        console.log(`finish index: ${i}, contractAddr: ${registryIndexArray[i]}`)
     }
 };
 
 func.id = "deploy_multi_index";
 func.tags = ["DeployMultiIndex"];
-func.dependencies = ["DeployComplianceRegistryStub_L1"];
+// func.dependencies = ["DeployComplianceRegistryStub_L1"];
 export default func;
--- deploy/007_deploy_multi_black_storage.ts
+++ deploy/007_deploy_multi_black_storage.ts
@@ -1,14 +1,13 @@
-import { DeployFunction } from "hardhat-deploy/types";
-import { HardhatRuntimeEnvironment } from "hardhat/types";
+import {DeployFunction} from "hardhat-deploy/types";
+import {HardhatRuntimeEnvironment} from "hardhat/types";
 import {ComplianceRegistryStub_L1, RegistryFactory, RegistryIndexFactory} from "../typechain-types";
-import {toNumber} from "ethers";
 import {ethers} from "hardhat";
 import fs from "fs";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { getNamedAccounts, deployments, ethers } = hre;
-    const { deploy } = deployments;
-    const { deployer } = await getNamedAccounts();
+    const {getNamedAccounts, deployments, ethers} = hre;
+    const {deploy} = deployments;
+    const {deployer} = await getNamedAccounts();
 
     const registryFactory = (await ethers.getContract(
         "RegistryFactory"
@@ -25,35 +24,43 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     }
 
     /// blacklist
-    let blacklistArray = []
-    for (let i = 0; i < registryIndexArray.length; i++) {
-        for (let j = 0; j < 4; j++) {
+    // let blacklistArray = []
+    // for (let i = 0; i < registryIndexArray.length; i++) {
+    //     for (let j = 0; j < 4; j++) {
+    //         const address = await registryFactory.deploy.staticCall(
+    //             j,
+    //             registryIndexArray[i],
+    //             stubAddress,
+    //             false
+    //         );
+    //         blacklistArray.push(address);
+    //     }
+    // }
+    // const jsonData = JSON.stringify(blacklistArray, null, 2)
+    // fs.writeFileSync("blacklistArray.json", jsonData)
+
+    for (let i = 43; i < registryIndexArray.length; i++) {
+        for (let j = 0; j < 1; j++) {
             const address = await registryFactory.deploy.staticCall(
                 j,
                 registryIndexArray[i],
                 stubAddress,
                 false
             );
-            blacklistArray.push(address);
-        }
-    }
-    const jsonData = JSON.stringify(blacklistArray, null, 2)
-    fs.writeFileSync("blacklistArray.json", jsonData)
-
-    for (let i = 0; i < registryIndexArray.length; i++) {
-        for (let j = 0; j < 4; j++) {
             const tx = await registryFactory.deploy(
                 j,
                 registryIndexArray[i],
                 stubAddress,
-                false
+                false,
+                {gasPrice: 15544294392}
             );
             await tx.wait();
+            console.log(`finish blacklist index: ${i}, pivot: ${j}, contractAddr:  ${address}, using index: ${registryIndexArray[i]}`)
         }
     }
 };
 
 func.id = "deploy_multi_black_storage";
 func.tags = ["DeployMultiBlackStorage"];
-func.dependencies = ["DeployComplianceRegistryStub_L1"];
+// func.dependencies = ["DeployComplianceRegistryStub_L1"];
 export default func;
